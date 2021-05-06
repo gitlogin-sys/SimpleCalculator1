a14bdb2b81c0728e2e357fc827d3d312905bb636
KidusMT <kidusmamuye@gmail.com>
2021-05-06 11:18:22 -0500

    updated solution with html

--- src/main/java/miu/kidusmt/Calculator.java
+++ src/main/java/miu/kidusmt/Calculator.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 /**
  * @author KidusMT
@@ -14,21 +15,31 @@ public class Calculator extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//        super.doGet(req, resp);
         String addOp1 = req.getParameter("addOp1");
         String addOp2 = req.getParameter("addOp2");
 
         String multiOp1 = req.getParameter("multiOp1");
         String multiOp2 = req.getParameter("multiOp2");
 
+        PrintWriter out = resp.getWriter();
+
+        out.println("<!DOCTYPE html>");
+        out.println("<html lang='en'>");
+        out.println("<head>");
+        out.println("<meta charset='UTF-8'>");
+        out.println("<title>Simple Calculator</title>");
+        out.println("</head>");
+        out.println("<body>");
+
         if (!addOp1.isEmpty() && !addOp2.isEmpty()) {
             int addResult = Integer.parseInt(addOp1) + Integer.parseInt(addOp2);
             System.out.println("===> add result: " + addResult);
 
             String addData = String.format("%s + %s = %s", addOp1, addOp2, addResult);
-            req.setAttribute("addResp", addData);
-        }else{
-            req.setAttribute("addResp", "");
+
+            out.println("<h3>" + addData + "</h3>");
+        } else {
+            out.println("<h3></h3>");
         }
 
         if (!multiOp1.isEmpty() && !multiOp2.isEmpty()) {
@@ -36,11 +47,12 @@ public class Calculator extends HttpServlet {
             System.out.println("===> multi result: " + multiResult);
 
             String multiData = String.format("%s * %s = %s", multiOp1, multiOp2, multiResult);
-            req.setAttribute("multiResp", multiData);
-        }else{
-            req.setAttribute("multiResp", "");
+            out.println("<h3>" + multiData + "</h3>");
+        } else {
+            out.println("<h3></h3>");
         }
 
-        req.getRequestDispatcher("result.jsp").forward(req, resp);
+        out.println("</body>");
+        out.println("</html>");
     }
 }
